239822e122e85f1d3b16b117b70a3287ee3584e4
unknown <enter7377@naver.com>
2022-02-13 14:09:53 +0900

    style: added prettier setting

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<div class="flex flex-col min-h-screen min-w-full items-center justify-center">
+  <div class="flex flex-col rounded shadow-lg max-w-4xl w-full py-2">
+    <div class="flex flex-col">
+      <div class="flex flex-row">
+        <input type="text" class="border-b border-black" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </div>
+    </div>
+  </div>
+</div>
