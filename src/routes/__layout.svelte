17769ffc6eea0dcde26de8b4afbae12ad013918a
unknown <enter7377@naver.com>
2022-02-13 21:17:41 +0900

    reaf: og desc

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,6 +6,7 @@
   <title>Todos</title>
   <link rel="icon" href={"../../static/favicon.png"} type="image/png" />
   <meta property="og:title" content="Todos" />
+	<meta property="og:description" content="A Simple Todo App">
   <meta property="og:image" content="/favicon.png" />
   <meta name="keywords" content="todo,svelte" />
   <meta name="description" content="A Simple Todo App" />
