05e221daddca0924b4e0d1739db6441441f3ad45
unknown <enter7377@naver.com>
2022-02-13 21:35:35 +0900

    reaf: added metatags

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,16 +1,27 @@
 <script lang="ts">
   import "../app.css"
+  import {MetaTags} from "svelte-meta-tags"
 </script>
 
 <svelte:head>
-  <title>Todos</title>
   <link rel="icon" href={"../../static/favicon.png"} type="image/png" />
-  <meta property="og:title" content="Todos" />
-	<meta property="og:description" content="A Simple Todo App">
-  <meta property="og:image" content="/favicon.png" />
-  <meta name="keywords" content="todo,svelte" />
-  <meta name="description" content="A Simple Todo App" />
 </svelte:head>
+<MetaTags
+  title="Todos"
+  description="A Simple Todo App."
+  openGraph={{
+    title: "Todos",
+    description: "A Simple Todo App.",
+    images: [
+      {
+        url: "/favicon.png",
+        width: 1024,
+        height: 1024,
+      },
+    ],
+    site_name: "todos",
+  }}
+/>
 
 <main>
   <slot />
